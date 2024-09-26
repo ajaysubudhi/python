54d0593ba341ea2e21ae0a4a356cef2819348a96
ajaysubudhi <46310703+ajaysubudhi@users.noreply.github.com>
2024-09-26 01:27:23 -0700

    Create Count_Common_Words_With_One_Occurrence.py

---
+++ Count_Common_Words_With_One_Occurrence.py
@@ -0,0 +1,31 @@
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+Example 1:
+
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+Example 2:
+
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+Example 3:
+
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+
+===========================================
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        counter = 0
+        for i in words1 :
+            if i in words2 :
+                if words1.count(i) == words2.count(i) ==1:
+                    counter = counter + 1
+        return counter
